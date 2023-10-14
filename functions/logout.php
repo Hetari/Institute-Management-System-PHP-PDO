c638ebaa34d2723e0ca20e00b6659a2e40aafd28
Hetari <hetari4py@gmail.com>
2023-10-14 20:27:34 +0300

    fixing the logout.php

--- functions/logout.php
+++ functions/logout.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 require_once("code.php");
-
-if (isset($_POST['singoutbtn'])) {
-    if (isset($_SESSION['auth'])) {
-        unset($_SESSION['auth']);
-        unset($_SESSION['auth_user']);
-        $_SESSION['messsage'] = "Logged out Successfully";
-    }
-    re_direct("../index.php", "success", "Logging out...");
+if (isset($_SESSION['auth'])) {
+    unset($_SESSION['auth']);
+    unset($_SESSION['auth_user']);
+    $_SESSION['messsage'] = "Logged out Successfully";
 }
+re_direct("../index.php", "success", "Logging out...");
+
 
 session_destroy();
